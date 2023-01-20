c8eebedf1c6e6beef18c54bb0f325845159b861e
Joe Tonthat <monkeydjiotaro@gmail.com>
2023-01-20 18:28:22 +0700

    update dockerizing ingest_data.py + execution script

---
+++ wk_1_basics_n_setup/2_docker_sql/run-ingest.sh
@@ -0,0 +1,10 @@
+# load env from file
+source $(dirname $0)/.env
+
+# echo $PG_CONN_STRING 
+# echo $DATA_TABLE_NAME 
+# echo $DATA_URL
+
+# run dockerize script with loaded env
+docker run -it --rm --network=pg-network dez23/taxi_ingest:0.0.1 \
+  --conn=$PG_CONN_STRING --table_name=$DATA_TABLE_NAME --data_url=$DATA_URL
