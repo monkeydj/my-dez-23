c9f971a51c251c844009c83325834359c9cd8dc0
Joe Tonthat <monkeydjiotaro@gmail.com>
2023-02-08 21:56:29 +0700

    [hw] change to deploy simple flow script

--- wk_3_data_warehouse/deploy_flow.py
+++ wk_3_data_warehouse/deploy_flow.py
@@ -1,15 +1,12 @@
 from prefect.deployments import Deployment
-from prefect.filesystems import GitHub
 from elt_web_to_gcs import elt_batch
-
-github_block = GitHub.load("dez-github")
 
 deployment = Deployment.build_from_flow(
     flow=elt_batch,
-    name="elt-data-warehouse-github",
-    storage_block=github_block,
+    name="elt-data-warehouse-hw",
     tags=["homework", "wk3"],
-    parameters={"color": "fhv", "year": 2019}
+    parameters={"color": "fhv", "year": 2019},
+    skip_upload=True,
 )
 
 
